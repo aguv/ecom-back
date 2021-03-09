102742ed3f4532066895e4eae03f73702401c98f
aguv <agufvazquez@gmail.com>
2021-03-09 14:52:05 -0300

    products routes working

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const Sequelize = require("sequelize");
 const {PORT, HOST_NAME, DB_NAME} = process.env;
 
-const db = new Sequelize(`postgres://${HOST_NAME}:${PORT}/${DB_NAME}`, {
+const db = new Sequelize(`postgres://${HOST_NAME}/${DB_NAME}`, {
   logging: false,
 });
 
